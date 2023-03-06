6ad1ee81745d4808f52d4f15f252b7044ab8dacb
Chigozie Johnpaul Maduka <chigoziemaduka401@gmail.com>
2023-03-06 19:42:21 +0100

    commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const colorSelector = document.getElementById('color-selector')
+const modeSelector = document.getElementById('mode-selector')
+const colorDisplay = document.getElementById('color-display')
+const getColorSchemeBtn = document.getElementById('get-color-scheme-btn')
+const hexValues = document.getElementById('hex-values')
+
+
+getColorSchemeBtn.addEventListener('click', e => {
+    e.preventDefault()
+    /* Gets color scheme from 'thecolorapi' */
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorSelector.value.replace(/#/g, '')}&mode=${modeSelector.value}`)
+        .then(response => response.json())
+        .then(data => {
+            let html = ''
+            data.colors.forEach(info => {
+                html += `
+                        <div>
+                            <h2 style="background-color: ${info.hex.value};"></h2>
+                            <p id="hex-values">${info.hex.value}</p>
+                        </div> 
+                        `
+                })
+                colorDisplay.innerHTML = html
+            })
+    })
+    
+// hexValues.addEventListener('click', () => {
+//     navigator.clipboard.writeText(hexValues.textContent)
+// })
