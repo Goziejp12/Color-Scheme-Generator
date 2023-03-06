44030b9dc8c1b812e02c0acc044c5ce1394d0279
Chigozie Johnpaul Maduka <chigoziemaduka401@gmail.com>
2023-03-06 20:31:05 +0100

    commit

--- index.js
+++ index.js
@@ -2,24 +2,7 @@ const colorSelector = document.getElementById('color-selector')
 const modeSelector = document.getElementById('mode-selector')
 const colorDisplay = document.getElementById('color-display')
 const getColorSchemeBtn = document.getElementById('get-color-scheme-btn')
-const formDisplay = document.getElementById('form-dislay')
 
-formDisplay.innerHTML = 
-                `
-                <input type="color" id="color-selector">
-                <select id="mode-selector">
-                    <option disabled selected>Select color mode</option>
-                    <option value="monochrome">monochrome</option> 
-                    <option value="monochrome-dark">monochrome-dark</option> 
-                    <option value="monochrome-light">monochrome-light</option> 
-                    <option value="analogic">analogic</option> 
-                    <option value="complement">complement</option> 
-                    <option value="analogic-complement">analogic-complement</option> 
-                    <option value="triad">triad</option> 
-                    <option value="quad">quad</option>
-                </select>
-                <button id="get-color-scheme-btn">Get color scheme</button>
-                `
 
 getColorSchemeBtn.addEventListener('click', e => {
     e.preventDefault()
