90af7aaf10c3ad278c0f47daa7b63bd0ad336cdd
Chigozie Johnpaul Maduka <chigoziemaduka401@gmail.com>
2023-03-07 17:26:04 +0100

    made the hex code to be copied to clipboard

--- index.js
+++ index.js
@@ -15,10 +15,21 @@ getColorSchemeBtn.addEventListener('click', e => {
                 html += `
                         <div>
                             <h2 style="background-color: ${info.hex.value};"></h2>
-                            <p>${info.hex.value}</p>
+                            <p id="hex-code" onclick="copyHexCode()">${info.hex.value}</p>
                         </div> 
                         `
                 })
                 colorDisplay.innerHTML = html
             })
     })
+    
+    function copyHexCode() {
+        let textRange = document.createRange()
+            textRange.selectNode(document.getElementById('hex-code'))
+            window.getSelection().removeAllRanges() //clear current selection
+            window.getSelection().addRange(textRange) // to select text
+            document.execCommand("copy")
+            window.getSelection().removeAllRanges() // to deselect
+            alert("data copied")
+    } 
+    
